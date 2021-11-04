a6e8141b2f42f10f1bfad172e0753ec673fa2933
Dylan Lathrum <dylanlathrum@gmail.com>
2021-11-04 03:57:29 -0700

    Update setup script to set up development environment and imaging environment

--- code/scripts/setup.sh
+++ code/scripts/setup.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env sh
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "$parent_path"
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; cd .. ; pwd -P )
 
 echo "Initializing git submodules..."
 git submodule update --init --recursive
 echo "Installing dependencies..."
-sudo pacman -S opus expat openssl alsa-lib avahi libevdev cmake base-devel --noconfirm
-sudo pacman -S manjaro-arm-tools manjaro-tools-base --noconfirm
-sudo getarmprofiles -f
+sudo pacman -S opus expat openssl alsa-lib avahi libevdev cmake base-devel --needed --noconfirm
+sudo pacman -S manjaro-arm-tools manjaro-tools-base --needed --noconfirm
+sudo getarmprofiles
 echo "Creating symlinks..."
-ln -s ../arm-profile/editions/thesis /usr/share/manjaro-arm-tools/profiles/arm-profiles/editions/thesis
-ln -s ../arm-profile/overlays/thesis /usr/share/manjaro-arm-tools/profiles/arm-profiles/overlays/thesis
-ln -s ../arm-profile/services/thesis /usr/share/manjaro-arm-tools/profiles/arm-profiles/services/thesis
+sudo ln -s $parent_path/arm-profile/editions/thesis /usr/share/manjaro-arm-tools/profiles/arm-profiles/editions/thesis
+sudo ln -s $parent_path/arm-profile/overlays/thesis /usr/share/manjaro-arm-tools/profiles/arm-profiles/overlays/thesis
+sudo ln -s $parent_path/arm-profile/services/thesis /usr/share/manjaro-arm-tools/profiles/arm-profiles/services/thesis
 # read -s -n 1 -p "Press any key to continue . . ."
